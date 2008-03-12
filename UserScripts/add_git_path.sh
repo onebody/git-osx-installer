298c62b89b7fbd6ff1c25e0ac6c99e74562224b2
timcharper <timcharper@025266bc-2e48-0410-9a2f-a18bf4baccf4>
2008-03-12 08:37:27 +0000

    Refined installation source.  add_git_path.sh now modifies MANPATH as well, along with ~/.bash_profile.  Removed executable settings for git hooks in the template directory (was enabling the hooks by default on newly created git repos... not fun)

--- UserScripts/add_git_path.sh
+++ UserScripts/add_git_path.sh
@@ -1,31 +1,52 @@
 #!/bin/sh
 ruby=`which ruby /opt/local/bin/ruby /usr/local/bin/ruby | uniq`
 echo $ruby
-$ruby <<-EOF
+$ruby <<-EOF 2>&1 > /tmp/git_path_output.txt
   require 'fileutils'
   GIT_DIR = "/usr/local/git/bin"
+  GIT_MAN_DIR = "/usr/local/git/man"
 
-  dir = "#{ENV['HOME']}/.MacOSX/"
-  FileUtils.mkdir_p(dir)
-
-  puts "modifying the MacOSX environment.plist file"
-  Dir.chdir(dir) do 
-    paths = if File.exist?("environment.plist")
-      %x{defaults read ~/.MacOSX/environment PATH}.strip.split(":")
-    else
-      ENV["PATH"].split(":")
-    end
-    paths << GIT_DIR
-    path = paths.uniq * ":"
-    puts %x{defaults write ~/.MacOSX/environment PATH "#{path}"}
-  end
-
-  puts "Modifying ~/.profile"
-  Dir.chdir(ENV["HOME"]) do
-    profile = File.exist?(".profile") ? File.read(".profile") : ""
-    unless /PATH.+#{Regexp.escape(GIT_DIR)}/.match(profile)
-      profile << "\nexport PATH=#{GIT_DIR}:\$PATH\n"
-      File.open(".profile", "wb") { |f| f << profile }
+  ENV_PLIST_DIR = "#{ENV['HOME']}/.MacOSX/"
+  FileUtils.mkdir_p(ENV_PLIST_DIR)
+  
+  def append_plist_var(name, append_value, default_value = nil)
+    Dir.chdir(ENV_PLIST_DIR) do 
+      puts "1"
+      values = File.exist?("environment.plist") ? %x{defaults read ~/.MacOSX/environment #{name}}.strip.split(":") : []
+      puts "2 - #{ENV.inspect} - #{name}"
+      values = (ENV[name] || default_value || "").split(":") if values.empty?
+      puts "3"
+      values << append_value
+      puts "4"
+      output = values.uniq * ":"
+      puts "5"
+      puts %x{defaults write ~/.MacOSX/environment #{name} "#{output}"}
     end
   end
+  
+  def append_script_var(name, append_value)
+    [".profile", ".bash_profile"].each do |filename|
+      Dir.chdir(ENV["HOME"]) do
+        profile = File.exist?(filename) ? File.read(filename) : ""
+        unless /#{name}.+#{Regexp.escape(append_value)}/.match(profile)
+          profile << "\nexport #{name}=#{append_value}:\$#{name}\n"
+          File.open(filename, "wb") { |f| f << profile }
+        end
+      end
+    end
+  end
+  
+  puts "modifying the MacOSX environment.plist PATH variable"
+  append_plist_var("PATH", GIT_DIR, "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/opt/local/bin")
+  
+  puts "modifying the MacOSX environment.plist MANPATH variable"
+  append_plist_var("MANPATH", GIT_MAN_DIR, "/usr/local/man:/usr/share/man:/usr/local/share/man:/usr/X11/man")
+  
+  puts "modifying PATH variable"
+  append_script_var("PATH", GIT_DIR)
+  
+  puts "modifying MANPATH variable"
+  append_script_var("MANPATH", GIT_MAN_DIR)
+  
+  puts "All done!"
 EOF
