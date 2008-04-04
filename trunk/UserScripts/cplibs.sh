c00f5e2a7d616433934e135af3be42ce6232590c
timcharper <timcharper@025266bc-2e48-0410-9a2f-a18bf4baccf4>
2008-04-04 17:20:49 +0000

    Small modifications to make the symlinks copy correctly on my machine (-R wasn't doing it for me ;P)

--- trunk/UserScripts/cplibs.sh
+++ trunk/UserScripts/cplibs.sh
@@ -4,11 +4,17 @@ TO_DIR="/usr/local/git/lib/"
 
 mkdir -p $TO_DIR
 pushd $FROM_DIR
-cp -R libcrypto.0.9.8.dylib libexpat.dylib \
-			libz.1.2.3.dylib libcrypto.dylib \
-			libssl.0.9.8.dylib libz.1.dylib \
-			libexpat.1.5.2.dylib libssl.dylib $TO_DIR
+cp -R libcrypto.0.9.8.dylib \
+			libz.1.2.3.dylib \
+			libssl.0.9.8.dylib \
+			libexpat.1.5.2.dylib $TO_DIR
 
 cd $TO_DIR
 strip *.dylib
+
+ln -s libcrypto.0.9.8.dylib libcrypto.dylib
+ln -s libz.1.2.3.dylib libz.1.dylib
+ln -s libssl.0.9.8.dylib libssl.dylib
+ln -s libexpat.1.5.2.dylib libexpat.dylib
+
 popd
