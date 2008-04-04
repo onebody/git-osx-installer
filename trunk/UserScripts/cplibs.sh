fb16e1b0abaef49b0fbc486b85abc98e3c37070b
timcharper <timcharper@025266bc-2e48-0410-9a2f-a18bf4baccf4>
2008-04-04 08:33:23 +0000

    added line to tell cplibs.sh to strip all dylib files (reduces file size a bit by removing debug symbols)

--- trunk/UserScripts/cplibs.sh
+++ trunk/UserScripts/cplibs.sh
@@ -3,10 +3,12 @@ FROM_DIR="/opt/local/lib/"
 TO_DIR="/usr/local/git/lib/"
 
 mkdir -p $TO_DIR
-cd $FROM_DIR
+pushd $FROM_DIR
 cp -R libcrypto.0.9.8.dylib libexpat.dylib \
 			libz.1.2.3.dylib libcrypto.dylib \
 			libssl.0.9.8.dylib libz.1.dylib \
 			libexpat.1.5.2.dylib libssl.dylib $TO_DIR
-	
-cd $OLDPWD
+
+cd $TO_DIR
+strip *.dylib
+popd
